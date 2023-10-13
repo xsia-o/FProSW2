12fd1f24dc69558a090d2730918f9cafea14f5fe
Mario Alexander <aleph.xsia@outlook.com>
2023-10-12 23:21:19 -0500

    Avance #1

--- src/screens/FrontPage.js
+++ src/screens/FrontPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 function FrontPage({ onCards, onExpenses, onAccount, onLogoff }) {
   return (
--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 function HomePage({ onNavigate }) {
   return (
     <div>
-      <h1>Bienvenido a mi aplicación</h1>
-      <button onClick={() => onNavigate()}>Comenzar</button>
+      <Stack direction="column" justifyContent="center" alignItems="center" spacing={2}>
+      <h1>Bienvenido a FPro</h1>
+      <Button variant="outlined" onClick={() => onNavigate()}>Comenzar</Button> 
+      </Stack>
+      
     </div>
   );
 }
